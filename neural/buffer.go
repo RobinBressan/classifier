8c09e8b333b4cb9ae3fdd34173f5990091f92997
Robin Bressan <robin@buddey.net>
2014-12-03 23:26:37 +0100

    Remove threshold and level from buffer

--- neural/buffer.go
+++ neural/buffer.go
@@ -3,18 +3,12 @@ package neural
 type Buffer struct {
     data []float64
     indexes []bool
-    threshold int
-    level int
     onComplete func([] float64)
 }
 
 func (b *Buffer) set(index int, datum float64) {
     b.data[index] = datum
-
-    if (!b.indexes[index]) {
-        b.indexes[index] = true
-        b.level += index
-    }
+    b.indexes[index] = true
 
     for _, i := range b.indexes {
         if !i {
@@ -29,6 +23,5 @@ func (b *Buffer) set(index int, datum float64) {
 func (b *Buffer) Truncate() {
     for i := 0; i < len(b.indexes); i++ {
         b.indexes[i] = false
-        b.level = 0
     }
 }
--- neural/buffer_factory.go
+++ neural/buffer_factory.go
@@ -6,13 +6,6 @@ func (b *BufferFactory) create(size int, onComplete func([] float64)) *Buffer {
     buffer := new(Buffer)
     buffer.data = make([]float64, size)
     buffer.indexes = make([]bool, size)
-    buffer.level = 0
-
-    buffer.threshold = 0
-    for i := 0; i < size; i++ {
-        buffer.threshold += i
-    }
-
     buffer.onComplete = onComplete
 
     return buffer

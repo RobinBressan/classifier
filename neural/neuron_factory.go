24d19e2accc497e804cd22248d6a5eb8fd17177e
Robin Bressan <robin@buddey.net>
2014-12-04 09:18:47 +0100

    Add todo

--- neural/neuron_factory.go
+++ neural/neuron_factory.go
@@ -4,6 +4,7 @@ type NeuronFactory struct {
     Id int
 }
 
+// @TODO rename to NewNeuron
 func (f *NeuronFactory) Create(inputs []*Synapse, outputs []*Synapse, compute func(float64) float64) *Neuron {
     f.Id++
     return &Neuron{ Id: f.Id, Inputs: inputs, Outputs: outputs, Compute: compute }
